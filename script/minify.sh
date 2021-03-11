b0ca3918e620326150e033944b3afdcddae2a780
amireshoon <abtin.yas53@gmail.com>
2021-03-11 23:18:14 +0330

    Some changes

--- script/minify.sh
+++ script/minify.sh
@@ -16,10 +16,8 @@ export PATH=$PATH:/usr/local/bin/minifier
 echo $1
 echo $2
 
-git config --global user.name "amireshoon"
-
 mkdir github && cd github
-git clone git@github.com:$2.git workspace
+git clone https://github.com/$2.git workspace
 cd workspace
 ls
 # echo $2
