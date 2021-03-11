623f3b77cf951fbe5b52b6a2050c7b11c960aaca
amireshoon <abtin.yas53@gmail.com>
2021-03-11 23:51:12 +0330

    Some changes

--- script/minify.sh
+++ script/minify.sh
@@ -18,9 +18,17 @@ git clone https://github.com/$2.git workspace
 cd workspace
 ls
 
-FILES= exec find -name '*.css'
-for f in FILES
+CSS=$(find . -type f -name "*.css")
+JS=$(find . -type f -name "*.js")
+
+for c in $CSS
 do
-echo "proc $f"
+echo "doint css -> $c"
+# minifier $f
+done
+
+for j in $JS
+do
+echo "doint js -> $j"
 # minifier $f
 done
