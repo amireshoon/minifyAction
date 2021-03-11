cfec93b51eb54c6fe8039c39d78174414e84e4d9
amireshoon <abtin.yas53@gmail.com>
2021-03-12 00:10:12 +0330

    Some changes

--- script/minify.sh
+++ script/minify.sh
@@ -9,6 +9,7 @@ sudo mv /usr/local/bin/minifier.py /usr/local/bin/minifier
 
 # making executable
 sudo chmod +x /usr/local/bin/minifier
+sudo chmod u+rx minifier.sh
 
 # exporting to path
 export PATH=$PATH:/usr/local/bin/minifier
