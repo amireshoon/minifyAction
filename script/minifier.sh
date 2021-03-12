b960028f92d296a0f1c3e5c68118b223df3947f2
amireshoon <abtin.yas53@gmail.com>
2021-03-12 16:28:17 +0330

    Some changes

--- script/minifier.sh
+++ script/minifier.sh
@@ -25,7 +25,7 @@ done
 echo "Minified $ITEMS files"
 
 git config --global user.name 'MinifyActionbot'
-git config --global user.email '<>'
+git config --global user.email '<abtin.yas53@gmail.com>'
 git status
 git switch -c $1
 git add -A
