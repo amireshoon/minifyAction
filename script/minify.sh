1723ce4f71a9e3ca0569e71f312ca7652d941648
amireshoon <abtin.yas53@gmail.com>
2021-03-12 14:40:05 +0330

    Some changes

--- script/minify.sh
+++ script/minify.sh
@@ -7,11 +7,16 @@ pip install css-html-js-minify
 sudo cp minifier.py /usr/local/bin
 sudo mv /usr/local/bin/minifier.py /usr/local/bin/minifier
 
+sudo cp minifier.sh /usr/local/bin
+sudo mv /usr/local/bin/minifier.sh /usr/local/bin/minifierbash
+
 # making executable
 sudo chmod +x /usr/local/bin/minifier
 sudo chmod u+rx minifier.sh
 
 # exporting to path
 export PATH=$PATH:/usr/local/bin/minifier
+
+export PATH="/usr/local/bin/minifierbash:$PATH"
 
 echo "MinifyAcion is ready"
