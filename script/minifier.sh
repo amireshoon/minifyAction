981577eb99901b3696fc2d7349e3121dce0878d2
amireshoon <abtin.yas53@gmail.com>
2021-03-12 17:31:28 +0330

    Some changes

--- script/minifier.sh
+++ script/minifier.sh
@@ -32,11 +32,41 @@ echo "Minified $ITEMS files"
 # git commit -m "Minified css, js and html"
 # git push origin $1
 
-git config user.name "amireshoon"
-git config user.email "abtin.yas53@gmail.com"
-cd github/workspace
-git status
+# git config user.name "amireshoon"
+# git config user.email "abtin.yas53@gmail.com"
+# cd github/workspace
+# git status
+# git switch -c $1
+# git add -A
+# git commit -m "Minified css, js and html"
+# git push "https://$4:$3@github.com/$2.git" $1
+
+
+
+set -e
+
+timestamp=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
+
+INPUT_AUTHOR_EMAIL='github-actions[bot]@users.noreply.github.com'
+INPUT_AUTHOR_NAME='github-actions[bot]'
+
+REPOSITORY=$2
 git switch -c $1
+echo "Push to branch $1";
+[ -z "$3" ] && {
+    echo 'Missing input "github_token: ${{ secrets.GITHUB_TOKEN }}".';
+    exit 1;
+};
+
+cd github/workspace
+
+remote_repo="https://$4:$3@github.com/$2.git"
+
+git config http.sslVerify false
+git config --local user.email "${INPUT_AUTHOR_EMAIL}"
+git config --local user.name "${INPUT_AUTHOR_NAME}"
+
 git add -A
-git commit -m "Minified css, js and html"
-git push "https://$4:$3@github.com/$2.git" $1
+
+git commit -m "minified"
+git push "${remote_repo}" HEAD:"$1";
