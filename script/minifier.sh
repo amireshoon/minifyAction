7cf91212d535916cb5cc58a17056e42b2af49dba
Amirhossein Meydani <36164960+amireshoon@users.noreply.github.com>
2021-03-14 21:57:09 +0330

    Added html, htm support for minifier

--- script/minifier.sh
+++ script/minifier.sh
@@ -10,8 +10,14 @@ CSS=$(find . -type f -name "*.css")
 # Getting all js files in project
 JS=$(find . -type f -name "*.js")
 
+# Getting all html files in project
+HTML=$(find . -type f -name ".*html")
+# Getting all htm files in project
+HTM=$(find . -type f -name ".*htm")
+
 # For counting all files that minified
 ITEMS=0
+
 for c in $CSS
 do
 # Sending path to minifier.py
@@ -24,6 +30,22 @@ for j in $JS
 do
 # Sending path to minifier.py
 minifier $j
+# Counting this files in items
+ITEMS=$(($ITEMS + 1))
+done
+
+for h in $HTML
+do
+# Sending path to minifier.py
+minifier $h
+# Counting this files in items
+ITEMS=$(($ITEMS + 1))
+done
+
+for hm in $HTM
+do
+# Sending path to minifier.py
+minifier $hm
 # Counting this files in items
 ITEMS=$(($ITEMS + 1))
 done
